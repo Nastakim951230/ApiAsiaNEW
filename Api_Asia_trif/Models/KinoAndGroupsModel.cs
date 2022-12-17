2d530c228dfb12b6138e3f6e8004cdfe8500a5be
Настя Ким <nastytrifono03@gmail.com>
2022-12-17 09:03:20 +0300

    кино и группа

--- Api_Asia_trif/Models/KinoAndGroupsModel.cs
+++ Api_Asia_trif/Models/KinoAndGroupsModel.cs
@@ -7,16 +7,29 @@ namespace Api_Asia_trif.Models
 {
     public class KinoAndGroupsModel
     {
+        private AsiaDrama_TrifonovaEntities db = new AsiaDrama_TrifonovaEntities();
         public KinoAndGroupsModel(KinoAndGroups kinoAndGroups)
         {
             IdKinoAndGroups = kinoAndGroups.IdKinoAndGroups;
             IdUsers=kinoAndGroups.IdUsers;
             IdKino=kinoAndGroups.IdKino;
             IdGroups=kinoAndGroups.IdGroups;
+
+            Name = db.KinoAndSerial.ToList().FirstOrDefault(y => y.IdKinoAndSerial == kinoAndGroups.IdKino).Name;
+
+            YearKinoAndSerial = db.KinoAndSerial.ToList().FirstOrDefault(y => y.IdKinoAndSerial == kinoAndGroups.IdKino).YearKinoAndSerial;
+            PhotoKinoAndSerial = db.KinoAndSerial.ToList().FirstOrDefault(y => y.IdKinoAndSerial == kinoAndGroups.IdKino).PhotoKinoAndSerial;
+            KinoAndSerial kino = db.KinoAndSerial.FirstOrDefault(x => x.IdKinoAndSerial == kinoAndGroups.IdKino);
+            OsnovnoeGanr = db.Genre.ToList().FirstOrDefault(y => y.IdGenre == kino.OsnovnoeGanr).NameGenre;
         }
         public int IdKinoAndGroups { get; set; }
         public int IdUsers { get; set; }
         public int IdKino { get; set; }
         public int IdGroups { get; set; }
+
+        public string Name { get; set; }
+        public int YearKinoAndSerial { get; set; }
+        public string PhotoKinoAndSerial { get; set; }
+        public string OsnovnoeGanr { get; set; }
     }
 }
